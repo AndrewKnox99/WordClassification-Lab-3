9c3bd373c155f2170363bb5694d583feb9cc5758
Andrew Knox <knoxa17@wfu.edu>
2019-09-25 14:38:51 -0400

    Implemented a method that returns the Flesh-Kincaid Reading Score metric of a given string input.

--- WordClassification_Lab_3.py
+++ WordClassification_Lab_3.py
@@ -2,9 +2,15 @@
 Andrew Knox
 CSC 391 - Data Mining
 Lab 3 - Word Classification
+
+Keyword extraction: USE RAKE ALGORITHM, from nltk (natural language tool kit)
+
 """
 
+import re
+
 #Retrieved from StackOverflow: https://stackoverflow.com/questions/46759492/syllable-count-in-python
+#Takes in a single word string, not a sentence, and returns the syllable count of the string
 def syllable_count(word):
     word = word.lower()
     count = 0
@@ -19,3 +25,23 @@ def syllable_count(word):
     if count == 0:
         count += 1
     return count
+
+#Method returns the Flesch reading ease score of a passage
+#Params: syllableCount -> Number of syllables in passage
+#        wordCount     -> Number of words in passage
+#        sentenceCount -> Number of sentences in passage
+def fleschEaseScore(sentenceCount, wordCount, syllableCount):
+    score = 206.835 - 1.015 * (wordCount/sentenceCount) - 84.6 * (syllableCount/wordCount)
+    return score
+
+#Takes in a long string passage, then returns a tuple containg:
+#[Sentence count, word count, syllable count] for the passage
+def getValuesForFleshScore(longInput):
+    sentenceCount, wordCount, syllableCount = 0, 0, 0
+    longInput = re.split(' |\n', longInput)
+    for word in longInput:
+        if word.endswith('.'):
+            sentenceCount += 1
+        wordCount += 1
+        syllableCount += syllable_count(word)
+    return (sentenceCount, wordCount, syllableCount)
